d934a298864cb60f3daae0266302e57d4ffddd50
lalli051811 <neeruduvijayalaxmi53@gmail.com>
2024-09-23 19:12:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+name = "Neeru"
+age = 14
+is_student = True
+weight = 45
+
+print("Name :", name)
+print("Data Type of Name is", type(nmae))
+print("Age :", age)
+print("Data Type of Age is", type(age))
+print("is_student :", is_student)
+print("Data Type of is_student is", type(weight))
+
+print("/n After Tye Casting....")
+age = str(age)
+print(age)
+print("data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of weight is", type(weight))
